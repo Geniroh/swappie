c70e25ad937f1b5984307c6cc94d2e21ce3981d0
Geniroh <irochibuzor@gmail.com>
2023-09-04 16:46:14 +0100

    added home response

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.get("/", (req,res,next) => {
+    try {
+        res.status(200).send("welcome to swappie API for more information check out our <a href='https://documenter.getpostman.com/view/15738942/2s9Y5eNKkK'>documentation</a>");
+    } catch (error) {
+        next(error)
+    }
+})
+
 app.use('/api/films', filmRoutes);
 
 app.use(errorHandlerMiddleware);
